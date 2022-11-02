47e32646c5ac29c98aca37335e967e54f24966cd
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-02 10:49:25 +0100

    additional failing test

--- bank/bankaccount.py
+++ bank/bankaccount.py
@@ -14,7 +14,16 @@ class BankAccount(BaseModel):
 
 def test1():
     account = BankAccount(owner='janko hrasko')
-    assert account.balance == 0
+    assert account.balance == 0, 'Balance should be 0 when account was created.'
 
 
-# test1()
+"""
+ked vlozim na prazdny ucet 100, tak zostatok bude 100
+"""
+def test2():
+    account = BankAccount(owner='janko hrasko')
+    account.credit(100)
+    assert account.balance == 100, 'Balance should be 100.'
+
+test1()
+test2()
