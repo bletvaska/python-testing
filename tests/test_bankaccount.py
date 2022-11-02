055c9cbab59ddbf291162eaa74502df695c309d7
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-02 14:46:24 +0100

    added fixture for empty bank account

--- tests/test_bankaccount.py
+++ tests/test_bankaccount.py
@@ -3,48 +3,53 @@ import pytest
 from bank.bankaccount import BankAccount
 
 
-def test_when_account_was_created_then_balance_is_0():
-    # arrange + act
+@pytest.fixture
+def empty_bankaccount():
     account = BankAccount(owner="janko hrasko")
+    yield account
 
+
+def test_when_account_was_created_then_balance_is_0(empty_bankaccount):
     # assert
-    assert account.balance == 0, "Balance should be 0 when account was created."
+    assert (
+        empty_bankaccount.balance == 0
+    ), "Balance should be 0 when account was created."
 
 
-def test_when_100_is_added_to_empty_account_then_balance_should_be_100():
-    # arrange
-    account = BankAccount(owner="janko hrasko")
-
+def test_when_100_is_added_to_empty_account_then_balance_should_be_100(
+    empty_bankaccount,
+):
     # act
-    account.deposit(100)
+    empty_bankaccount.deposit(100)
 
     # assert
-    assert account.balance == 100, "Balance should be 100."
+    assert empty_bankaccount.balance == 100, "Balance should be 100."
 
 
-def test_when_deposit_to_account_with_nonzero_balance_then_final_balance_is_higher():
+def test_when_deposit_to_account_with_nonzero_balance_then_final_balance_is_higher(
+    empty_bankaccount,
+):
     # arrange
     old_balance = 50
     deposit = 80
-    account = BankAccount(owner="janko hrasko", balance=old_balance)
+    empty_bankaccount.deposit(old_balance)
 
     # act
-    account.deposit(deposit)
+    empty_bankaccount.deposit(deposit)
 
     # assert
     assert (
-        account.balance == old_balance + deposit
+        empty_bankaccount.balance == old_balance + deposit
     ), f"Balance should be {old_balance + deposit}."
 
 
-def test_when_negative_amount_is_deposited_then_valueerror_exception_should_be_thrown():
+def test_when_negative_amount_is_deposited_then_valueerror_exception_should_be_thrown(empty_bankaccount):
     # arrange
     amount = -100
-    account = BankAccount(owner="sherlock holmes")
 
     # act + assert
     with pytest.raises(ValueError):
-        account.deposit(amount)
+        empty_bankaccount.deposit(amount)
 
 
 @pytest.mark.wip
@@ -60,10 +65,8 @@ def test_when_negative_amount_is_deposited_then_valueerror_exception_should_be_t
         (0,),
     ],
 )
-def test_when_non_integer_type_is_deposited_then_expect_typeerror_exception(amount):
-    # arrange
-    account = BankAccount(owner="emily holmes")
-
+def test_when_non_integer_type_is_deposited_then_expect_typeerror_exception(amount, empty_bankaccount):
     # act + assert
     with pytest.raises(TypeError):
-        account.deposit(amount)
+        empty_bankaccount.deposit(amount)
+
