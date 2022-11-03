f4c7e577b4ef14bc3907c1fde69e62429713272e
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-03 13:06:48 +0100

    addded fixture for testing with selenium

--- tests/python.org/test_pythonorg.py
+++ tests/python.org/test_pythonorg.py
@@ -3,8 +3,31 @@ from selenium.webdriver.common.by import By
 import pytest
 
 
-@pytest.mark.wip
-def test_when_url_is_opened_then_python_should_be_in_title():
+@pytest.fixture(scope="module")
+def driver():
     driver = webdriver.Chrome()
-    driver.get('https://www.python.org')
-    assert 'Python' in driver.title
+    yield driver
+    driver.quit()
+
+
+def test_when_url_is_opened_then_python_should_be_in_title(driver):
+    # arrange / act
+    driver.get("https://www.python.org")
+
+    # assert
+    assert "Python" in driver.title
+
+
+def test_when_string_is_searched_then_results_must_be_labeled_in_h3(faker, driver):
+    # arrange
+    driver.get("https://www.python.org")
+
+    # act
+    # search for search bar
+    element = driver.find_element(By.NAME, "q")
+    element.send_keys(faker.sentence())
+    element.submit()
+
+    # assert
+    element = driver.find_element(By.TAG_NAME, "h3")
+    assert element.text == "Results"
