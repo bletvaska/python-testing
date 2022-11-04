84b9517ea8c05eb3639e31292ceaaa72b01fec4b
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-04 10:56:51 +0100

    additional tests

--- tests/movies/test_unauthorized.py
+++ tests/movies/test_unauthorized.py
@@ -3,22 +3,49 @@ import requests
 
 def test_when_request_without_keys_then_expect_status_code_401():
     # arrange
-    url = 'https://parseapi.back4app.com/classes/movies'
+    url = "https://parseapi.back4app.com/classes/movies"
+    expected = 401
 
     # act
     response = requests.get(url)
 
     # assert
-    assert response.status_code == 401
+    assert response.status_code == expected
 
 
 def test_when_request_without_keys_then_expect_error_message():
     # arrange
-    url = 'https://parseapi.back4app.com/classes/movies'
-    expected = {'error': 'unauthorized'}
+    url = "https://parseapi.back4app.com/classes/movies"
+    expected = {"error": "unauthorized"}
 
     # act
     response = requests.get(url)
+
+    # assert
+    assert response.json() == expected
+
+
+def test_when_valid_parse_app_id_was_provided_then_expect_status_code_403():
+    # arrange
+    url = "https://parseapi.back4app.com/classes/movies"
+    headers = {"X-Parse-Application-Id": "axACcyh0MTO3z42rUN8vFHfyAgE22VRjd3IJOwlJ"}
+    expected = 403
+
+    # act
+    response = requests.get(url, headers=headers)
+
+    # assert
+    assert response.status_code == expected
+
+
+def test_when_valid_parse_app_id_was_provided_then_expect_error_message():
+    # arrange
+    url = "https://parseapi.back4app.com/classes/movies"
+    headers = {"X-Parse-Application-Id": "axACcyh0MTO3z42rUN8vFHfyAgE22VRjd3IJOwlJ"}
+    expected = {"error": "unauthorized"}
+
+    # act
+    response = requests.get(url, headers=headers)
 
     # assert
     assert response.json() == expected
