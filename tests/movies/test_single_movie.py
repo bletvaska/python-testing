e283480f208c4e7db1f79f9ce4c85738426a5fe1
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-04 13:09:20 +0100

    additional test suite for testing single movie

---
+++ tests/movies/test_single_movie.py
@@ -0,0 +1,49 @@
+import pytest
+import requests
+
+
+def test_when_request_without_keys_then_expect_status_code_401(base_url):
+    # arrange
+    url = f"{base_url}/classes/movies/WHyejDIzEv"
+    expected = 401
+
+    # act
+    response = requests.get(url)
+
+    # assert
+    assert response.status_code == expected
+
+
+def test_when_valid_keys_are_provided_then_expect_status_code_200(base_url, headers):
+    # arrange
+    url = f"{base_url}/classes/movies/WHyejDIzEv"
+    expected = 200
+
+    # act
+    response = requests.get(url, headers=headers)
+
+    # assert
+    assert response.status_code == expected
+
+
+def test_when_valid_keys_are_provided_then_expect_content_of_type_json(base_url, headers):
+    # arrange
+    url = f"{base_url}/classes/movies/WHyejDIzEv"
+
+    # act
+    response = requests.get(url, headers=headers)
+
+    # assert
+    assert response.headers['Content-Type'].startswith('application/json')
+
+
+@pytest.mark.parametrize("key",['objectId', 'title', 'year', 'genres', 'createdAt', 'updatedAt'])
+def test_when_valid_keys_are_provided_then_expect_keys_in_json(base_url, headers, key):
+    # arrange
+    url = f"{base_url}/classes/movies/WHyejDIzEv"
+
+    # act
+    response = requests.get(url, headers=headers)
+
+    # assert
+    assert key in response.json()
