f9dcd5fe9d2f4d09ef560c51330032c23c166612
Miroslav Biňas <miroslav.binas@tuke.sk>
2022-11-04 14:48:07 +0100

    created tests as classes

--- tests/movies/test_movie_creation.py
+++ tests/movies/test_movie_creation.py
@@ -1,10 +1,20 @@
 import pytest
 
 
-def test_when_movie_is_created_then_http_status_code_should_be_201(movie):
-    assert movie.status_code == 201
+class TestMovieCreation:
+    def test_when_movie_is_created_then_http_status_code_should_be_201(self, movie):
+        assert movie.status_code == 201
+
+    @pytest.mark.parametrize("key", ["objectId", "createdAt"])
+    def test_when_movie_is_created_then_it_should_pass_through_jsonschema_validation(self, movie, key):
+        assert key in movie.json()
 
 
-@pytest.mark.parametrize("key", ["objectId", "createdAt"])
-def test_when_movie_is_created_then_it_should_pass_through_jsonschema_validation(movie, key):
-    assert key in movie.json()
+class TestMovieDelete:
+    def test_when_movie_is_deleted_then_http_status_code_should_be_204(self):
+        assert True
+
+
+class TestMovieUpdate:
+    def test_when_movie_is_updated_then_http_status_code_should_be_200(self):
+        assert True
