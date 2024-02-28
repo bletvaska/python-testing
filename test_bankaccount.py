1c15523e5d38216d4e49bf2660c6f3871368a3d3
mirek <mirek@cnl.sk>
2024-02-28 14:44:33 +0100

    refaktoring

--- test_bankaccount.py
+++ test_bankaccount.py
@@ -14,7 +14,13 @@ class BankAccount(BaseModel):
         if amount <= 0:
             raise ValueError("Invalid amount.")
 
-        self.balance = amount
+        self.balance += amount
+
+    def withdraw(self, amount: int):
+        if self.balance - amount <= 0:
+            raise ValueError('Not enough money.')
+
+        self.balance -= amount
 
 
 @pytest.fixture()
@@ -48,8 +54,23 @@ def test_when_invalid_type_of_amount_then_type_error_should_be_raised(amount, ac
     with pytest.raises(TypeError):
         account.deposit(amount)
 
+
 # ked vyberiem 100 evry, tak na ucte budem mat o 100 evry menej
+def test_when_withdraw_100_then_balance_should_be_100_evry_less(account):
+    # arrange
+    account.deposit(200)
+
+    # act
+    account.withdraw(100)
+
+    # assert
+    assert account.get_balance() == 100
+
+
 # ked vyberiem viac ako je zostatok, tak ValueError
+def test_when_no_funds_on_account_when_withdraw_then_valueerror_exception_should_be_raised(account):
+    with pytest.raises(ValueError):
+        account.withdraw(10)
+
 # ked zadam zapornu hodnotu, tak vynimka ValueError
 # ked zadam iny typ ako cele cislo, tak TypeError
-
