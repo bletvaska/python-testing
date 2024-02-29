901cb73feb7135cd4dfaf7f647e0edee92769393
mirek <mirek@cnl.sk>
2024-02-29 14:11:54 +0100

    dalsi test pre testovanie python.org

--- tests/pythonorg/test_homepage.py
+++ tests/pythonorg/test_homepage.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 def test_when_on_homepage_then_python_in_title():
@@ -8,5 +9,16 @@ def test_when_on_homepage_then_python_in_title():
 
     # assert
     assert 'Python' in driver.title, 'No Python in title.'
+
+    driver.close()
+
+
+def test_when_on_homepage_then_searchbar_is_present():
+    # arrange
+    driver = webdriver.Chrome()
+    driver.get('https://www.python.org')
+
+    # act / assert
+    driver.find_element(By.ID, 'id-search-field')
 
     driver.close()
