961fd968dbbc5d649c47aa6ee3f1a60c55f16895
mirek <mirek@cnl.sk>
2024-02-29 14:39:52 +0100

    pridany fixture pre browser

--- tests/pythonorg/test_homepage.py
+++ tests/pythonorg/test_homepage.py
@@ -1,40 +1,33 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-def test_when_on_homepage_then_python_in_title():
-    # arrange
+@pytest.fixture
+def browser():
     driver = webdriver.Chrome()
     driver.get('https://www.python.org')
 
+    yield driver
+
+    driver.close()
+
+
+def test_when_on_homepage_then_python_in_title(browser):
     # assert
-    assert 'Python' in driver.title, 'No Python in title.'
-
-    driver.close()
+    assert 'Python' in browser.title, 'No Python in title.'
 
 
-def test_when_on_homepage_then_searchbar_is_present():
-    # arrange
-    driver = webdriver.Chrome()
-    driver.get('https://www.python.org')
-
+def test_when_on_homepage_then_searchbar_is_present(browser):
     # act / assert
-    driver.find_element(By.ID, 'id-search-field')
-
-    driver.close()
+    browser.find_element(By.ID, 'id-search-field')
 
 
-def test_when_search_string_entered_then_results_must_show_on_page():
-    # arrange
-    driver = webdriver.Chrome()
-    driver.get('https://www.python.org')
-
+def test_when_search_string_entered_then_results_must_show_on_page(browser):
     # act
-    element = driver.find_element(By.ID, 'id-search-field')
+    element = browser.find_element(By.ID, 'id-search-field')
     element.send_keys('pycon')
     element.submit()
 
     # assert
-    driver.find_element(By.TAG_NAME, 'h2')
-
-    driver.close()
+    browser.find_element(By.TAG_NAME, 'h2')
