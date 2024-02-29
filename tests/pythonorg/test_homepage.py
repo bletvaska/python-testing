c908966aacbcebc078257f59a499daeb0784ebc3
mirek <mirek@cnl.sk>
2024-02-29 14:26:13 +0100

    dalsi test pre testovanie python.org

--- tests/pythonorg/test_homepage.py
+++ tests/pythonorg/test_homepage.py
@@ -22,3 +22,19 @@ def test_when_on_homepage_then_searchbar_is_present():
     driver.find_element(By.ID, 'id-search-field')
 
     driver.close()
+
+
+def test_when_search_string_entered_then_results_must_show_on_page():
+    # arrange
+    driver = webdriver.Chrome()
+    driver.get('https://www.python.org')
+
+    # act
+    element = driver.find_element(By.ID, 'id-search-field')
+    element.send_keys('pycon')
+    element.submit()
+
+    # assert
+    driver.find_element(By.TAG_NAME, 'h2')
+
+    driver.close()
