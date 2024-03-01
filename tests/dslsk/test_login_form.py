7aa517e1074c1980f7932af785acf483eec219c0
mirek <mirek@cnl.sk>
2024-03-01 12:31:25 +0100

    pridany pytest-selenium

--- tests/dslsk/test_login_form.py
+++ tests/dslsk/test_login_form.py
@@ -7,44 +7,40 @@ pytestmark = [
 ]
 
 
-@pytest.fixture
-def login_page(browser):
-    browser.get('https://www.dsl.sk/user.php?action=login')
-    yield browser
-
-
 # ak zadam spravny login a spravne heslo tak ma presmeruje na stranku https://www.dsl.sk/ .
-def test_if_valid_login_and_password_entered_then_user_is_logged_in(login_page, config):
+def test_if_valid_login_and_password_entered_then_user_is_logged_in(selenium, variables):
     # arrange
-    username = login_page.find_element(By.NAME, 'login')
-    username.send_keys(config['username'])
-    password = login_page.find_element(By.NAME, 'password')
-    password.send_keys(config['password'])
+    selenium.get('https://www.dsl.sk/user.php?action=login')
+    username = selenium.find_element(By.NAME, 'login')
+    username.send_keys(variables['dsl']['username'])
+    password = selenium.find_element(By.NAME, 'password')
+    password.send_keys(variables['dsl']['password'])
 
     # act
     button_xpath = '/html/body/table/tbody/tr/td/div/table/tbody/tr[1]/td[2]/div/table/tbody/tr/td/form/input[7]'
-    login_page.find_element(By.XPATH, button_xpath).click()
+    selenium.find_element(By.XPATH, button_xpath).click()
 
     # assert
     # assert login_page.current_url == 'https://www.dsl.sk/'
-    element = login_page.find_element(By.XPATH, '/html/body/table/tbody/tr/td[1]/div/div[2]/table/tbody/tr/td[1]')
-    assert f'prihlásený: {config["username"]}' in element.text
+    element = selenium.find_element(By.XPATH, '/html/body/table/tbody/tr/td[1]/div/div[2]/table/tbody/tr/td[1]')
+    assert f'prihlásený: {variables["dsl"]["username"]}' in element.text
 
     # cleanup
-    login_page.get('https://www.dsl.sk/user.php?action=logout')
+    selenium.get('https://www.dsl.sk/user.php?action=logout')
 
 
 @pytest.mark.wip
-def test_when_no_password_entered_then_not_logged_in(login_page, config):
+def test_when_no_password_entered_then_not_logged_in(selenium, variables):
     # arrange
-    username = login_page.find_element(By.NAME, 'login')
-    username.send_keys(config['username'])
+    selenium.get('https://www.dsl.sk/user.php?action=login')
+    username = selenium.find_element(By.NAME, 'login')
+    username.send_keys(variables['dsl']['username'])
 
     # act
     button_xpath = '/html/body/table/tbody/tr/td/div/table/tbody/tr[1]/td[2]/div/table/tbody/tr/td/form/input[7]'
-    login_page.find_element(By.XPATH, button_xpath).click()
+    selenium.find_element(By.XPATH, button_xpath).click()
 
     # assert
-    element = login_page.find_element(By.XPATH,
+    element = selenium.find_element(By.XPATH,
                                       '/html/body/table/tbody/tr/td/div/table/tbody/tr[1]/td[2]/div/table/tbody/tr/td/form/b')
     assert 'Nesprávne meno alebo heslo alebo Váš email nebol zatiaľ overený.' in element.text
